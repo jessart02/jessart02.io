0bb320aa7d51f2ee4eec19d1f56bffbff0119fe4
jessart02 <59274az@gmail.com>
2026-01-11 23:50:48 -0600

    jj

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,9 @@ import react from '@astrojs/react';
 export default defineConfig({
   site: 'https://jessart02.github.io',
   base: '/jessart02.io/',
+  integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
 });
